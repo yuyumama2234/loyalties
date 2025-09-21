5ed5039b2eaf46bd640632663d0763bc25a028d1
yuyumama <madridyuanajita07@gmail.com>
2025-09-21 19:06:43 +0800

    Added homepage

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
         <title>GIT Activity</title>
     </head>
     <body>
-        Team Name: LMK
+        Team Name: Loyalties
         Team Member: Domingo Jayv Miguel
                      Sonio Rey Daniel
                      Dairocas Dan Lyser
                      Ajita Yuan
+
     </body>
 </html>
