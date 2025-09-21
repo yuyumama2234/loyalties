4531de5ea060cba9a0dcc51e2139390063f8acbd
yuyumama <madridyuanajita07@gmail.com>
2025-09-21 18:51:05 +0800

    Added homepage file with php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>GIT Activity</title>
+    </head>
+    <body>
+        Team Name: LMK
+        Team Member: Domingo Jayv Miguel
+                     Sonio Rey Daniel
+                     Dairocas Dan Lyser
+                     Ajita Yuan
+    </body>
+</html>
