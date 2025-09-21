0b1c9faaf764618d24aa065340e2de15caf58960
yuyumama <madridyuanajita07@gmail.com>
2025-09-21 19:18:31 +0800

    Added homepage

--- home.php
+++ home.php
@@ -1,1 +1,14 @@
 
+<html>
+    <head>
+        <title>GIT Activity</title>
+    </head>
+    <body>
+        Team Name: Loyalties
+        Team Member: Domingo Jayv Miguel
+                     Sonio Rey Daniel
+                     Dairocas Dan Lyser
+                     Ajita Yuan
+
+    </body>
+</html>
